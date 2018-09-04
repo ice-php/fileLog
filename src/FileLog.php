1d3d491d503d56ae2d5af7d7d7c89b0498c72b8e
lanbing <liyong@nenu.edu.cn>
2018-09-04 15:43:11 +0800

    记录文本日志

--- src/FileLog.php
+++ src/FileLog.php
@@ -77,49 +77,7 @@ final class FileLog
         $this->makeDir(dirname($path));
 
         // 写入文件
-        return $this->writeFile($path, $msg, FILE_APPEND | LOCK_EX);
-    }
-
-    /**
-     * 对file_put_contents的封装,以修正文件所有者
-     * @param $file string 文件名
-     * @param $content string 文件内容
-     * @param int $flag FILE_APPEND&|LOCK_EX
-     * @return false|int
-     */
-    private function writeFile(string $file, $content, int $flag = 0)
-    {
-        //当前用户
-        $current = getenv('USERNAME') ?: getenv('USER');
-
-        //应该是这个用户
-        $should = Config::get('system', 'OS_USER');
-
-        //如果操作系统是Windows或当前已经是应该的用户,则不处理
-        if (isWindows() or $current === $should) {
-            return file_put_contents($file, $content, $flag);
-        }
-
-        //提前调整文件所有者
-        if (function_exists('posix_getpwuid') && is_file($file)) {
-            $owner = posix_getpwuid(fileowner($file));
-            if ($owner !== $should) {
-                chown($file, $should);
-            }
-        }
-
-        //写入文件
-        $ret = file_put_contents($file, $content, $flag);
-
-        if (function_exists('posix_getpwuid')) {
-            //延后调整文件所有者
-            $owner = posix_getpwuid(fileowner($file));
-            if ($owner !== $should) {
-                chown($file, $should);
-            }
-        }
-
-        return $ret;
+        return write($path, $msg, FILE_APPEND | LOCK_EX);
     }
 
     /**
